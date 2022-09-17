cb20f5b39012f08ed2b495e7ba833082e61eb01f
zzuhann <zzuhanlin@gmail.com>
2022-09-17 12:40:15 +0800

    layout: pet diary detail(pc ver)

--- src/pages/PetDiaries/AllPetDiraies.tsx
+++ src/pages/PetDiaries/AllPetDiraies.tsx
@@ -1,59 +1,109 @@
 import { collection, getDocs, query } from "firebase/firestore";
 import React, { useState, useEffect } from "react";
-import { useDispatch, useSelector } from "react-redux";
 import { Link } from "react-router-dom";
 import styled from "styled-components";
-import { Profile } from "../../reducers/profile";
 import { db } from "../../utils/firebase";
+import { FontAwesomeIcon } from "@fortawesome/react-fontawesome";
+import { faHeart, faComment } from "@fortawesome/free-solid-svg-icons";
+
+const PageTitle = styled.h1`
+  font-size: 32px;
+  font-weight: bold;
+  position: absolute;
+  top: 70px;
+  padding-left: 12px;
+  &:before {
+    content: "";
+    width: 4px;
+    height: 100%;
+    background-color: #d0470c;
+    position: absolute;
+    left: 0;
+  }
+`;
+
+const Wrap = styled.div`
+  width: 100%;
+  min-height: 100vh;
+  height: 100%;
+  background-color: #fafafa;
+  position: relative;
+`;
 
 const AllDiariesContainer = styled.div`
-  max-width: 1200px;
+  max-width: 1120px;
   margin: 0 auto;
-  padding: 70px 0 46px;
+  padding: 150px 0 46px;
   display: flex;
   flex-wrap: wrap;
   transition: 0.3s;
+  position: relative;
+  justify-content: space-between;
 `;
-const DiaryCard = styled(Link)<{ likecount: number; commentcount: number }>`
+const DiaryCard = styled(Link)`
   width: 300px;
-  height: 300px;
-  margin: 0 20px 50px;
+  margin-bottom: 30px;
+  transition: 0.2s;
+  bottom: 0;
   flex-shrink: 0;
   display: flex;
   flex-direction: column;
   text-decoration: none;
   color: black;
+  border-radius: 10px;
+  overflow: hidden;
   position: relative;
-  &:hover:before {
-    content: "喜歡 ${(props) => props.likecount} 
-    留言 ${(props) => props.commentcount}";
-    position: absolute;
-    width: 100%;
-    height: 100%;
-    background-color: rgba(0, 0, 0, 0.5);
-    color: #fff;
-    display: flex;
-    justify-content: center;
-    align-items: center;
-    text-align: center;
+  &:last-child {
+    margin-right: 0;
+  }
+  &:hover {
+    box-shadow: 5px 5px 4px 3px rgba(0, 0, 0, 0.2);
+    bottom: 5px;
+  }
+`;
+const LikeRecord = styled.div`
+  display: flex;
+  margin-right: 30px;
+  &:last-child {
+    margin-right: 0;
+  }
+  svg {
+    margin-right: 10px;
     font-size: 25px;
   }
 `;
-const DiaryImg = styled.img`
+
+const HoverCard = styled.div<{ $isActive: boolean }>`
+  background-color: rgba(0, 0, 0, 0.5);
+  color: #fff;
+  font-size: 22px;
+  position: absolute;
   width: 100%;
   height: 100%;
+  display: flex;
+  justify-content: center;
+  align-items: center;
+  opacity: ${(props) => (props.$isActive ? 1 : 0)};
+  transition: 0.2s;
+`;
+
+const DiaryImg = styled.img`
+  width: 300px;
+  height: 300px;
   object-fit: cover;
 `;
 const DiaryBottom = styled.div`
   display: flex;
   justify-content: space-between;
   position: absolute;
-  background-color: rgba(0, 0, 0, 0.5);
+  background-color: rgba(0, 0, 0, 0.3);
   color: #fff;
-  bottom: 0;
+  bottom: 1px;
   left: 0;
   width: 100%;
-  font-size: 24px;
+  padding: 10px 15px;
+  font-size: 22px;
+  letter-spacing: 1.5px;
 `;
 const DiaryTitle = styled.div``;
 const PetAge = styled.div``;
@@ -76,10 +126,9 @@ export type AllPetDiariesType = {
 };
 
 const AllPetDiaries = () => {
-  const profile = useSelector<{ profile: Profile }>(
-    (state) => state.profile
-  ) as Profile;
   const [allPetDiraies, setAllPetArticles] = useState<AllPetDiariesType[]>([]);
+  const [openLikeBox, setOpenLikeBox] = useState<boolean>(false);
+  const [nowid, setNowId] = useState<number>(-1);
 
   async function getPetDiaries() {
     const authorPetDiaries: AllPetDiariesType[] = [];
@@ -99,22 +148,41 @@ const AllPetDiaries = () => {
   }, []);
 
   return (
-    <AllDiariesContainer>
-      {allPetDiraies.map((diary, index) => (
-        <DiaryCard
-          key={index}
-          to={`/petdiary/${diary.id}`}
-          likecount={diary.likedBy.length}
-          commentcount={diary.commentCount}
-        >
-          <DiaryImg src={diary.img} />
-          <DiaryBottom>
-            <DiaryTitle>{diary.petName}</DiaryTitle>
-            <PetAge>{`${new Date().getFullYear() - diary.birthYear}`}Y</PetAge>
-          </DiaryBottom>
-        </DiaryCard>
-      ))}
-    </AllDiariesContainer>
+    <Wrap>
+      <AllDiariesContainer>
+        <PageTitle>寵物日記</PageTitle>
+        {allPetDiraies.map((diary, index) => (
+          <DiaryCard
+            key={index}
+            to={`/petdiary/${diary.id}`}
+            onMouseEnter={() => {
+              setOpenLikeBox(true);
+              setNowId(index);
+            }}
+            onMouseLeave={() => {
+              setOpenLikeBox(false);
+              setNowId(-1);
+            }}
+          >
+            <HoverCard $isActive={openLikeBox === true && nowid === index}>
+              <LikeRecord>
+                <FontAwesomeIcon icon={faHeart} /> {diary.likedBy.length}
+              </LikeRecord>
+              <LikeRecord>
+                <FontAwesomeIcon icon={faComment} /> {diary.commentCount}
+              </LikeRecord>
+            </HoverCard>
+            <DiaryImg src={diary.img} />
+            <DiaryBottom>
+              <DiaryTitle>{diary.petName}</DiaryTitle>
+              <PetAge>
+                {`${new Date().getFullYear() - diary.birthYear}`}Y
+              </PetAge>
+            </DiaryBottom>
+          </DiaryCard>
+        ))}
+      </AllDiariesContainer>
+    </Wrap>
   );
 };
 
--- src/pages/PetDiaries/DiaryDetail.tsx
+++ src/pages/PetDiaries/DiaryDetail.tsx
@@ -16,70 +16,137 @@ import styled from "styled-components";
 import { Profile } from "../../reducers/profile";
 import { db } from "../../utils/firebase";
 import { Link, useNavigate, useParams } from "react-router-dom";
-import notyetLike from "./like.png";
-import alreadyLike from "./like (1).png";
+import notyetLike from "./NotLike.png";
+import alreadyLike from "./AlreadyLike.png";
+import comment from "./chat.png";
 import { AllPetDiariesType } from "./AllPetDiraies";
 import { CommentType } from "../Articles/ArticleDetail";
+import { Btn } from "../ProfileSetting/UserInfos";
+
+const Wrap = styled.div`
+  width: 100%;
+  min-height: 100vh;
+  height: 100%;
+  background-color: #fafafa;
+  position: relative;
+`;
 
 const DiaryContainer = styled.div`
   display: flex;
   max-width: 1120px;
-  margin: 30px auto;
+  margin: 0 auto;
+  position: relative;
+  top: 80px;
+  border: solid 1px #dbdbdb;
+  border-radius: 10px;
+  overflow: hidden;
+  font-size: 15px;
 `;
+
 const DiaryImage = styled.img`
   width: 500px;
   height: 500px;
   object-fit: cover;
-  margin-right: 20px;
 `;
+
 const DiaryTextInfo = styled.div`
   display: flex;
   flex-direction: column;
   flex: 1;
+  background-color: #fff;
 `;
+
 const DiaryAuthorContainer = styled(Link)`
   display: flex;
+  border-bottom: 1px solid #efefef;
+  padding: 10px 15px;
+  position: relative;
 `;
+
+const DiaryAuthorContainerNoBorder = styled(Link)`
+  display: flex;
+  padding: 10px 15px 0 0;
+  position: relative;
+`;
+
 const AuthorImg = styled.img`
-  width: 50px;
-  height: 50px;
-  border-radius: 25px;
+  width: 40px;
+  height: 40px;
+  border-radius: 20px;
   object-fit: cover;
 `;
-const AuthorName = styled.div``;
+const AuthorName = styled.div`
+  position: relative;
+  display: flex;
+  align-items: center;
+  margin-left: 15px;
+  font-size: 15px;
+  font-weight: bold;
+  color: #3c3c3c;
+`;
 const MainSection = styled.div`
   display: flex;
   flex-direction: column;
   height: 300px;
   overflow-y: scroll;
+  padding: 0 15px 0 15px;
 `;
-const PetInfo = styled.div``;
+
 const DiaryContext = styled.div`
-  font-size: 20px;
-  margin-top: 15px;
+  font-size: 15px;
+  margin-left: 55px;
   margin-bottom: 15px;
 `;
+const TimeContainer = styled.div`
+  display: flex;
+  margin-left: 55px;
+  font-size: 12px;
+  color: #7d7d7d;
+`;
+
 const PostTime = styled.div``;
+const TakeptTime = styled.div`
+  margin-left: 20px;
+`;
+
+const CommentTitle = styled.div``;
+
 const CommentContainer = styled.div`
   display: flex;
   flex-direction: column;
   margin-top: 20px;
+  padding-bottom: 20px;
+  border-bottom: 1px solid #efefef;
+  word-break: break-all;
 `;
+
 const CommentCard = styled.div`
   position: relative;
+  margin-top: 15px;
 `;
 const RecordContainer = styled.div`
   display: flex;
+  align-items: center;
+  position: relative;
+  left: 15px;
+  padding-top: 15px;
+  padding-bottom: 15px;
 `;
 const RecordImg = styled.img`
-  width: 30px;
-  height: 30px;
+  width: 25px;
+  height: 25px;
 `;
 const Record = styled.div`
   margin-right: 15px;
+  margin-left: 5px;
+  font-size: 15px;
+  position: relative;
+  bottom: 3px;
 `;
+
 const CommentUserContainer = styled.div`
   display: flex;
+  align-items: center;
 `;
 const CommentUserImg = styled.img`
   width: 40px;
@@ -87,29 +154,41 @@ const CommentUserImg = styled.img`
   border-radius: 20px;
   object-fit: cover;
 `;
-const CommentUserName = styled.div``;
+const CommentUserName = styled.div`
+  position: relative;
+  display: flex;
+  align-items: center;
+  margin-left: 15px;
+  font-weight: bold;
+`;
 const CommentTime = styled.div`
   position: absolute;
   right: 5px;
   top: 5px;
 `;
-const CommentContext = styled.div``;
+const CommentContext = styled.div`
+  margin-left: 55px;
+  padding-right: 10px;
+  line-height: 20px;
+`;
 const AddComment = styled.div`
   display: flex;
   flex-direction: column;
+  padding: 0 15px 0 15px;
 `;
 const AddCommentTextArea = styled.textarea`
   resize: vertical;
+  min-height: 60px;
+  max-height: 80px;
   width: 100%;
+  border: 3px solid #efefef;
+  border-radius: 5px;
+  padding: 10px 15px;
+  font-size: 18px;
 `;
-const AddCommentBtn = styled.div`
-  text-align: center;
-  margin-top: 10px;
-  cursor: pointer;
-  &:hover {
-    background-color: #000;
-    color: #fff;
-  }
+const AddCommentBtn = styled(Btn)`
+  bottom: 100px;
+  right: 20px;
 `;
 
 const DiaryDetail = () => {
@@ -216,91 +295,106 @@ const DiaryDetail = () => {
 
   if (!targetDiary) return null;
   return (
-    <DiaryContainer>
-      <DiaryImage src={targetDiary.img} />
-      <DiaryTextInfo>
-        <DiaryAuthorContainer to={`/profile/${targetDiary.authorUid}`}>
-          <AuthorImg src={targetDiary.author.img} />
-          <AuthorName>Author: {targetDiary.author.name}</AuthorName>
-        </DiaryAuthorContainer>
-        <MainSection>
-          <PetInfo>
-            主角: {targetDiary.petName}(
-            {`${new Date().getFullYear() - targetDiary.birthYear}`}Y)
-          </PetInfo>
-          <DiaryContext>{targetDiary.context}</DiaryContext>
-          <PostTime>
-            發布於 {new Date(targetDiary.postTime).getFullYear()}/
-            {new Date(targetDiary.postTime).getMonth() + 1}/
-            {new Date(targetDiary.postTime).getDate()}
-          </PostTime>
-          <PostTime>
-            拍攝於 {new Date(targetDiary.takePhotoTime).getFullYear()}/
-            {new Date(targetDiary.takePhotoTime).getMonth() + 1}/
-            {new Date(targetDiary.takePhotoTime).getDate()}
-          </PostTime>
-          <CommentContainer>
-            留言區
-            {diaryComments.map((comment, index) => (
-              <CommentCard key={index}>
-                <CommentUserContainer>
-                  <CommentUserImg src={comment.user.img} />
-                  <CommentUserName>{comment.user.name}</CommentUserName>
-                </CommentUserContainer>
-                <CommentTime>
-                  {new Date(comment.commentTime).getFullYear()}/
-                  {new Date(comment.commentTime).getMonth() + 1}/
-                  {new Date(comment.commentTime).getDate()}{" "}
-                  {new Date(comment.commentTime).getHours()}:
-                  {new Date(comment.commentTime).getMinutes()}
-                </CommentTime>
-                <CommentContext>{comment.context}</CommentContext>
-              </CommentCard>
-            ))}
-          </CommentContainer>
-        </MainSection>
-        <RecordContainer>
-          <Record>喜歡 {targetDiary.likedBy.length}</Record>
-          <Record>留言 {targetDiary.commentCount}</Record>
-          {targetDiary.likedBy.includes(profile.uid) ? (
-            <RecordImg
-              src={alreadyLike}
-              onClick={() => {
-                toggleLike();
+    <Wrap>
+      <DiaryContainer>
+        <DiaryImage src={targetDiary.img} />
+        <DiaryTextInfo>
+          <DiaryAuthorContainer to={`/profile/${targetDiary.authorUid}`}>
+            <AuthorImg src={targetDiary.author.img} />
+            <AuthorName>
+              {targetDiary.author.name}/ {targetDiary.petName}(
+              {`${new Date().getFullYear() - targetDiary.birthYear}`}y)
+            </AuthorName>
+          </DiaryAuthorContainer>
+          <MainSection>
+            <DiaryAuthorContainerNoBorder
+              to={`/profile/${targetDiary.authorUid}`}
+            >
+              <AuthorImg src={targetDiary.author.img} />
+              <AuthorName>{targetDiary.author.name}</AuthorName>
+            </DiaryAuthorContainerNoBorder>
+            <DiaryContext>{targetDiary.context}</DiaryContext>
+            <TimeContainer>
+              <PostTime>
+                發布於 {new Date(targetDiary.postTime).getFullYear()}/
+                {new Date(targetDiary.postTime).getMonth() + 1}/
+                {new Date(targetDiary.postTime).getDate()}
+              </PostTime>
+              <TakeptTime>
+                拍攝於 {new Date(targetDiary.takePhotoTime).getFullYear()}/
+                {new Date(targetDiary.takePhotoTime).getMonth() + 1}/
+                {new Date(targetDiary.takePhotoTime).getDate()}
+              </TakeptTime>
+            </TimeContainer>
+            <CommentContainer>
+              <CommentTitle>留言區</CommentTitle>
+              {diaryComments.map((comment, index) => (
+                <CommentCard key={index}>
+                  <CommentUserContainer>
+                    <CommentUserImg src={comment.user.img} />
+                    <CommentUserName>{comment.user.name}</CommentUserName>
+                  </CommentUserContainer>
+                  <CommentTime>
+                    {new Date(comment.commentTime).getFullYear()}/
+                    {new Date(comment.commentTime).getMonth() + 1}/
+                    {new Date(comment.commentTime).getDate()}{" "}
+                    {new Date(comment.commentTime).getHours()}:
+                    {new Date(comment.commentTime).getMinutes()}
+                  </CommentTime>
+                  <CommentContext>{comment.context}</CommentContext>
+                </CommentCard>
+              ))}
+            </CommentContainer>
+          </MainSection>
+          <RecordContainer>
+            {targetDiary.likedBy.includes(profile.uid) ? (
+              <>
+                <RecordImg
+                  src={alreadyLike}
+                  onClick={() => {
+                    toggleLike();
+                  }}
+                />
+                <Record>{targetDiary.likedBy.length}</Record>
+              </>
+            ) : (
+              <>
+                <RecordImg
+                  src={notyetLike}
+                  onClick={() => {
+                    toggleLike();
+                  }}
+                />
+                <Record>{targetDiary.likedBy.length}</Record>
+              </>
+            )}
+            <RecordImg src={comment} />
+            <Record>{targetDiary.commentCount}</Record>
+          </RecordContainer>
+          <AddComment>
+            <AddCommentTextArea
+              value={newCommentContext}
+              placeholder="新增留言 ..."
+              onChange={(e) => {
+                setNewCommentContext(e.target.value);
               }}
-            />
-          ) : (
-            <RecordImg
-              src={notyetLike}
+            ></AddCommentTextArea>
+            <AddCommentBtn
               onClick={() => {
-                toggleLike();
+                if (!profile.isLogged) {
+                  window.alert("留言需先登入，確認後導向登入頁面");
+                  navigate("/profile");
+                  return;
+                }
+                addDiaryComment();
               }}
-            />
-          )}
-        </RecordContainer>
-        <AddComment>
-          <AddCommentTextArea
-            value={newCommentContext}
-            placeholder="新增留言 ..."
-            onChange={(e) => {
-              setNewCommentContext(e.target.value);
-            }}
-          ></AddCommentTextArea>
-          <AddCommentBtn
-            onClick={() => {
-              if (!profile.isLogged) {
-                window.alert("留言需先登入，確認後導向登入頁面");
-                navigate("/profile");
-                return;
-              }
-              addDiaryComment();
-            }}
-          >
-            新增留言
-          </AddCommentBtn>
-        </AddComment>
-      </DiaryTextInfo>
-    </DiaryContainer>
+            >
+              新增留言
+            </AddCommentBtn>
+          </AddComment>
+        </DiaryTextInfo>
+      </DiaryContainer>
+    </Wrap>
   );
 };
 
