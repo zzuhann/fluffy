eec5555479ae993a004fd9a2806ffc04030f184d
zzuhann <zzuhanlin@gmail.com>
2022-09-18 20:45:12 +0800

    layout: pairing feature(pc ver)

--- src/pages/Dating/Dating.tsx
+++ src/pages/Dating/Dating.tsx
@@ -8,43 +8,150 @@ import {
 } from "../../functions/datingReducerFunction";
 import { Card, Dating, petCardInfo, InviteDating } from "../../reducers/dating";
 import api from "../../utils/api";
-import { auth, db } from "../../utils/firebase";
-import { collection, doc, getDoc, getDocs } from "firebase/firestore";
+import { db } from "../../utils/firebase";
+import { collection, getDocs } from "firebase/firestore";
 import PetCardDetail from "./PairingFeature";
-import { SettingPreference } from "./PairingFeature";
 import ConsiderPetDetail from "./ConsiderPet";
 import UpcomingList from "./UpcomingList";
 import TogglePairingTabs from "./TogglePairingTabs";
 import { ConsiderEverySinglePetCard } from "./ConsiderPet";
 import { Profile } from "../../reducers/profile";
-import { onAuthStateChanged } from "firebase/auth";
-import { setName, setImage } from "../../functions/profileReducerFunction";
-import {
-  checkIfLogged,
-  setProfileUid,
-} from "../../functions/profileReducerFunction";
+import { Btn } from "../ProfileSetting/UserInfos";
+import preferenceSet from "./preference.png";
+import { area } from "./constantInfo";
+import close from "./close.png";
+
+const Wrap = styled.div`
+  width: 100%;
+  min-height: 100vh;
+  height: 100%;
+  background-color: #fafafa;
+  position: relative;
+`;
 
 const Cards = styled.div`
   position: relative;
-  width: 500px;
+  width: 450px;
   left: 50%;
   top: 50%;
   transform: translate(-50%, -50%);
 `;
 
-const RequestMoreCardsBtn = styled.div`
-  text-align: center;
+const FilterContainer = styled.div`
+  display: flex;
+  position: absolute;
+  top: 115px;
+  left: 20px;
+  opacity: 0.85;
+  cursor: pointer;
+  letter-spacing: 1.5px;
+  &:hover {
+    opacity: 1;
+  }
+`;
+
+const FilterImg = styled.img`
+  width: 20px;
+  height: 20px;
+  margin-right: 10px;
+`;
+const FilterTitle = styled.div`
+  font-size: 15px;
   position: relative;
-  left: 50%;
-  top: 200px;
-  width: 200px;
-  transform: translate(-50%);
-  margin-bottom: 10px;
+  top: 2px;
+`;
+
+const CloseFilterBtn = styled.img`
+  position: absolute;
+  width: 15px;
+  height: 15px;
+  top: 15px;
+  right: 15px;
+  opacity: 0.8;
+  transition: 0.2s;
   cursor: pointer;
   &:hover {
-    background-color: #000;
-    color: #fff;
+    opacity: 1;
   }
+`;
+
+const FilterInsideContainer = styled.div`
+  position: absolute;
+  top: 90px;
+  left: 20px;
+  border-radius: 8px;
+  z-index: 999;
+  background-color: #fff;
+  padding: 15px;
+  font-size: 15px;
+  letter-spacing: 1.5px;
+  box-shadow: 2px 2px 4px 3px rgba(0, 0, 0, 0.2);
+  width: 350px;
+`;
+const FilterInfoContainer = styled.div`
+  display: flex;
+  flex-direction: column;
+  opacity: 0.85;
+  margin-top: 10px;
+  &:first-child {
+    margin-top: 0;
+  }
+`;
+const FilterInfoTitle = styled.div``;
+const FilterOptionGroup = styled.div`
+  display: flex;
+  flex-wrap: wrap;
+  margin-top: 8px;
+`;
+const FilterInfoOption = styled(Btn)`
+  width: auto;
+  font-size: 15px;
+  position: relative;
+  border: solid 2px #d1cfcf;
+  padding: 5px 15px;
+  transition: 0.3s;
+  margin-right: 8px;
+  margin-bottom: 8px;
+  color: #3c3c3c;
+  &:last-child {
+    margin-right: 0;
+  }
+  &:hover {
+    background-color: #b7b0a8;
+  }
+`;
+
+const KindOption = styled(FilterInfoOption)<{ $isActive: boolean }>`
+  background-color: ${(props) => (props.$isActive ? "#B7B0A8" : "#fff")};
+`;
+const AreaOption = styled(FilterInfoOption)<{ $isActive: boolean }>`
+  background-color: ${(props) => (props.$isActive ? "#B7B0A8" : "#fff")};
+`;
+
+const NoCardsTitle = styled.div`
+  font-size: 22px;
+  font-weight: bold;
+  color: #3c3c3c;
+  position: absolute;
+  top: 25%;
+  left: 50%;
+  transform: translateX(-50%);
+`;
+
+const NoCardsBtn = styled(Btn)`
+  width: 250px;
+`;
+
+const RequestMoreCardsBtn = styled(NoCardsBtn)`
+  top: 35%;
+  left: 50%;
+  transform: translateX(-50%);
+`;
+
+const LookConsiderListBtn = styled(NoCardsBtn)`
+  top: 45%;
+  left: 50%;
+  transform: translateX(-50%);
 `;
 
 const ConsiderList = styled.div`
@@ -77,11 +184,14 @@ const Pairing: React.FC = () => {
   const [considerDetail, setConsiderDetail] = useState<Boolean>(false);
   const [nowChosenPetIndex, setNowChosenPetIndex] = useState<number>(-1);
   const [tab, setTab] = useState<string>("pairing");
+  const [areaTabIndex, setAreaTabIndex] = useState<number>(-1);
+  const [kindTabIndex, setKindTabIndex] = useState<number>(-1);
+  const [openFilterBox, setOpenFilterBox] = useState<boolean>(false);
   const chosenIdRef = useRef<number[]>([]);
 
   useEffect(() => {
-    checkChosenAndAppendNewPet(50);
-  }, [preference]);
+    checkChosenAndAppendNewPet(100);
+  }, [preference, profile.uid]);
 
   async function checkChosenAndAppendNewPet(quantity: number) {
     await getListsData();
@@ -111,37 +221,34 @@ const Pairing: React.FC = () => {
   function pushCardsinAllCards(info: petCardInfo[], totalCards: number) {
     let cards: Card[] = [];
     let repeatId: number[] = [];
+
     for (let i = 0; i < totalCards; i++) {
-      if (
-        info[i]?.hasOwnProperty("animal_id") &&
-        info[i].animal_id &&
-        info[i]?.hasOwnProperty("album_file") &&
-        info[i].album_file
-      ) {
-        for (let j = 0; j < chosenIdRef.current.length; j++) {
-          if (chosenIdRef.current[j] === info[i].animal_id) {
-            repeatId.push(chosenIdRef.current[j]);
-          }
+      if (!info[i]) continue;
+      if (!info[i].hasOwnProperty("animal_id") || !info[i].animal_id) continue;
+      if (!info[i].hasOwnProperty("animal_id") || !info[i].album_file) continue;
+      for (let j = 0; j < chosenIdRef.current.length; j++) {
+        if (chosenIdRef.current[j] === info[i].animal_id) {
+          repeatId.push(chosenIdRef.current[j]);
         }
-        if (repeatId.includes(info[i].animal_id)) {
-          continue;
-        }
-        cards.push({
-          id: info[i].animal_id,
-          area: info[i].animal_area_pkid,
-          shelterName: info[i].animal_place,
-          shleterPkid: info[i].animal_shelter_pkid,
-          shelterAddress: info[i].shelter_address,
-          shelterTel: info[i].shelter_tel,
-          kind: info[i].animal_kind,
-          sex: info[i].animal_sex,
-          color: info[i].animal_colour,
-          sterilization: info[i].animal_sterilization,
-          foundPlace: info[i].animal_foundplace,
-          image: info[i].album_file,
-        });
-        dispatch(setAllCardInfrontOfUser(cards));
       }
+      if (repeatId.includes(info[i].animal_id)) {
+        continue;
+      }
+      cards.push({
+        id: info[i].animal_id,
+        area: info[i].animal_area_pkid,
+        shelterName: info[i].animal_place,
+        shleterPkid: info[i].animal_shelter_pkid,
+        shelterAddress: info[i].shelter_address,
+        shelterTel: info[i].shelter_tel,
+        kind: info[i].animal_kind,
+        sex: info[i].animal_sex,
+        color: info[i].animal_colour,
+        sterilization: info[i].animal_sterilization,
+        foundPlace: info[i].animal_foundplace,
+        image: info[i].album_file,
+      });
+      dispatch(setAllCardInfrontOfUser(cards));
     }
   }
 
@@ -178,8 +285,9 @@ const Pairing: React.FC = () => {
 
   if (!dating.allCards) return null;
   return (
-    <>
+    <Wrap>
       <TogglePairingTabs
+        tab={tab}
         setTab={setTab}
         getListsData={getListsData}
         setConsiderDetail={setConsiderDetail}
@@ -187,18 +295,17 @@ const Pairing: React.FC = () => {
       />
       {tab === "pairing" ? (
         <>
-          <SettingPreference
-            setPreference={setPreference}
-            preference={preference}
-          />
           {dating.allCards.length <= 0 ? (
             <>
+              <NoCardsTitle>
+                有發現喜歡的寵物嗎？接下來您可以選擇 ...
+              </NoCardsTitle>
               <RequestMoreCardsBtn
                 onClick={() => checkChosenAndAppendNewPet(200)}
               >
                 配對更多狗狗貓貓
               </RequestMoreCardsBtn>
-              <RequestMoreCardsBtn
+              <LookConsiderListBtn
                 onClick={() => {
                   setTab("considerAdopt");
                   getListsData();
@@ -206,10 +313,69 @@ const Pairing: React.FC = () => {
                 }}
               >
                 看目前考慮領養清單
-              </RequestMoreCardsBtn>
+              </LookConsiderListBtn>
             </>
           ) : (
             <Cards>
+              <FilterContainer onClick={() => setOpenFilterBox(true)}>
+                <FilterImg src={preferenceSet} />
+                <FilterTitle>偏好設定</FilterTitle>
+              </FilterContainer>
+              {openFilterBox ? (
+                <FilterInsideContainer>
+                  <CloseFilterBtn
+                    src={close}
+                    onClick={() => setOpenFilterBox(false)}
+                  />
+                  <FilterInfoContainer>
+                    <FilterInfoTitle>偏好種類</FilterInfoTitle>
+                    <FilterOptionGroup>
+                      <KindOption
+                        onClick={() => {
+                          setPreference({ ...preference, kind: "%E7%8B%97" });
+                          setKindTabIndex(0);
+                        }}
+                        $isActive={kindTabIndex === 0}
+                      >
+                        狗
+                      </KindOption>
+                      <KindOption
+                        onClick={() => {
+                          setPreference({ ...preference, kind: "%E8%B2%93" });
+                          setKindTabIndex(1);
+                        }}
+                        $isActive={kindTabIndex === 1}
+                      >
+                        貓
+                      </KindOption>
+                    </FilterOptionGroup>
+                  </FilterInfoContainer>
+                  <FilterInfoContainer>
+                    <FilterInfoTitle>偏好地區</FilterInfoTitle>
+                    <FilterOptionGroup>
+                      {area.map((loc, index) => (
+                        <AreaOption
+                          id={`${index + 2}`}
+                          key={index}
+                          onClick={(e) => {
+                            setPreference({
+                              ...preference,
+                              location: (e.target as HTMLElement).id,
+                            });
+                            setAreaTabIndex(index);
+                          }}
+                          $isActive={index === areaTabIndex}
+                        >
+                          {loc}
+                        </AreaOption>
+                      ))}
+                    </FilterOptionGroup>
+                  </FilterInfoContainer>
+                </FilterInsideContainer>
+              ) : (
+                ""
+              )}
+
               <PetCardDetail petInfo={dating.allCards} />
             </Cards>
           )}
@@ -242,7 +408,7 @@ const Pairing: React.FC = () => {
       ) : (
         ""
       )}
-    </>
+    </Wrap>
   );
 };
 export default Pairing;
--- src/pages/Dating/TogglePairingTabs.tsx
+++ src/pages/Dating/TogglePairingTabs.tsx
@@ -1,25 +1,49 @@
 import React from "react";
 import styled from "styled-components";
+import pairing from "./disguise.png";
+import consideradopt from "./cat.png";
+import upcomingdate from "./house.png";
 
 const TogglePairingTab = styled.div`
   display: flex;
   flex-direction: column;
-  width: 200px;
-  position: absolute;
-  top: 150px;
-  left: 10px;
+  justify-content: center;
+  align-items: center;
+  width: 250px;
+  position: fixed;
+  /* border: solid 1px black; */
+  background-color: #fff;
+  min-height: 100vh;
+  box-shadow: 1px 9px 5px 2px rgba(0, 0, 0, 0.2);
 `;
 
-const PairingTab = styled.div`
-  text-align: center;
+const PairingTabContainer = styled.div<{ $isActive: boolean }>`
+  display: flex;
+  align-items: center;
+  padding: 10px 30px;
+  width: 100%;
   cursor: pointer;
+  transition: 0.3s;
+  background-color: ${(props) => (props.$isActive ? "#b7b0a8" : "#fff")};
+  opacity: ${(props) => (props.$isActive ? "1" : "0.7")};
   &:hover {
-    background-color: #000;
-    color: #fff;
+    background-color: #b7b0a8;
+    opacity: 1;
   }
 `;
 
+const PairingTabIcon = styled.img`
+  width: 40px;
+  height: 40px;
+`;
+
+const PairingTab = styled.div`
+  letter-spacing: 1.5px;
+  margin-left: 15px;
+`;
+
 type Props = {
+  tab: string;
   setTab: (value: string) => void;
   getListsData: () => void;
   setConsiderDetail: (value: Boolean) => void;
@@ -29,24 +53,34 @@ type Props = {
 const TogglePairingTabs: React.FC<Props> = (props) => {
   return (
     <TogglePairingTab>
-      <PairingTab onClick={() => props.setTab("pairing")}>配對系統</PairingTab>
-      <PairingTab
+      <PairingTabContainer
+        onClick={() => props.setTab("pairing")}
+        $isActive={props.tab === "pairing"}
+      >
+        <PairingTabIcon src={pairing} />
+        <PairingTab>配對系統</PairingTab>
+      </PairingTabContainer>
+      <PairingTabContainer
         onClick={() => {
           props.setTab("considerAdopt");
           props.getListsData();
           props.setConsiderDetail(false);
         }}
+        $isActive={props.tab === "considerAdopt"}
       >
-        考慮領養清單
-      </PairingTab>
-      <PairingTab
+        <PairingTabIcon src={consideradopt} />
+        <PairingTab>考慮領養清單</PairingTab>
+      </PairingTabContainer>
+      <PairingTabContainer
         onClick={() => {
           props.setTab("upcomingDate");
           props.getUpcomingListData();
         }}
+        $isActive={props.tab === "upcomingDate"}
       >
-        即將到來的約會
-      </PairingTab>
+        <PairingTabIcon src={upcomingdate} />
+        <PairingTab>即將到來的約會</PairingTab>
+      </PairingTabContainer>
     </TogglePairingTab>
   );
 };
